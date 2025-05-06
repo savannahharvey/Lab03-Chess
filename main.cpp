0da237a562331850f8bee04721609b773e2c6d00
savannahharvey <ssammonstx@gmail.com>
2025-05-06 09:59:52 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/***********************************************************************
+ * Source File:
+ *    MAIN
+ * Author:
+ *    <your name here>
+ * Summary:
+ *    Launches the unit tests - that is all!
+ ************************************************************************/
+
+ #include "test.h"
+ #include "position.h"
+
+double Position::squareWidth = (double)SIZE_SQUARE;
+double Position::squareHeight = (double)SIZE_SQUARE;
+
+ /************************************
+  * MAIN
+  * There can be only one!
+  ************************************/
+int main()
+{
+   // run the test code.
+   testRunner();
+
+   // return success
+   return 0;
+}
